dac55655e144ca6d9428a986b581077367fa61a7
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-09-30 13:28:01 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.Red))
-let range = strip.range(0, 1)
+let strip: neopixel.Strip = null
+let range: neopixel.Strip = null
+basic.forever(function () {
+    strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    range = strip.range(0, 1)
+    strip.clear()
+    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+})
